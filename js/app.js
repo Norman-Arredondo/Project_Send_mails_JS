eb4be8fe89bc177cb2a99add8351553cb5752f53
Norman Arredondo <arredondo_norman25@outlook.es>
2022-08-22 23:41:43 -0500

    Validar campos
    Agregar borde con las classes de Tailwind si los campos están vacíos

--- js/app.js
+++ js/app.js
@@ -1,10 +1,24 @@
 /**Variables */
 const btnEnviar = document.querySelector('#enviar');
 
+
+/**Variables para campos */
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
+
+
 eventListeners();
 
 function eventListeners() {
+    // Cuando la app arranca
     document.addEventListener('DOMContentLoaded', iniciarApp);
+
+    // Campos del formulario
+    email.addEventListener('blur', validarFormulario); // blur se ejecuta cuando esté en el input y me salga
+    asunto.addEventListener('blur', validarFormulario); 
+    mensaje.addEventListener('blur', validarFormulario); 
+
 }
 
 
@@ -13,6 +27,17 @@ function eventListeners() {
 function iniciarApp() {
     btnEnviar.disabled = true;
     btnEnviar.classList.add('cursor-not-allowed', 'opacity-10');
+}
+
+//Valida el formulario
+function validarFormulario(e) {
+    
+    if(e.target.value.lenght > 0) {
+        console.log('Hay algo');
+    } else {
+        //e.target.style.borderBottomColor = 'red';
+        e.target.classList.add('border','border-red-500');
+    }
 }
 
 
